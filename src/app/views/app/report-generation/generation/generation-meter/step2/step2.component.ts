b36e54f4b5a46d35254c176a276576868841818a
softhabet <sofiene.thabet@etudiant-fst.utm.tn>
2022-03-11 10:28:49 +0100

    filter group done

--- src/app/views/app/report-generation/generation/generation-meter/step2/step2.component.ts
+++ src/app/views/app/report-generation/generation/generation-meter/step2/step2.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BsLocaleService} from 'ngx-bootstrap';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { esLocale } from 'ngx-bootstrap/locale';
 
 @Component({
   selector: 'app-step2',
@@ -37,11 +41,50 @@ export class Step2Component implements OnInit {
       { 'id': 5, 'name': 'meter_discovered' },
       { 'id': 6, 'name': 'meter_type' }
   ];
+  public filterList = [
+    { 'id': 1, 'name': 'status' },
+    { 'id': 2, 'name': 'pre_active_meter_date' },
+    { 'id': 3, 'name': 'meter_active_date' },
+    { 'id': 4, 'name': 'version' },
+    { 'id': 5, 'name': 'meter_discovered' },
+    { 'id': 6, 'name': 'meter_type' }
+];
+public valueList = [
+  { 'id': 1, 'name': 'equals' },
+  { 'id': 2, 'name': 'grater then' },
+  { 'id': 3, 'name': 'less then' },
+  { 'id': 4, 'name': 'not equal' },
+];
   selectedFieldId = 1;
+  selectedFilterId = 1;
+  selectedValueId = 1;
 
-  constructor() { }
+  // value or date
+  public value : boolean = false;
+  public date : boolean = true;
+
+  // DatePicker
+  form: FormGroup;
+  bsValue = new Date();
+  bsRangeValue: Date[];
+  maxDate = new Date();
+  mouseTime = new Date();
+
+  bsInlineValue = new Date();
+
+
+  constructor(private localeService: BsLocaleService) {
+    this.maxDate.setDate(this.maxDate.getDate() + 7);
+    this.bsRangeValue = [this.bsValue, this.maxDate];
+
+    defineLocale('es', esLocale);
+    // this.localeService.use('es');
+   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      basicDate: new FormControl(new Date()),
+    });
   }
 
 }
